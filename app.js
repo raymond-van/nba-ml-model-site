239e9470a9a0c6b10744f207f31a3e893bc732ab
raymond-van <vraymond028@gmail.com>
2019-02-11 19:28:30 -0500

    NBA site v1

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID
+
+const app = express();
+
+var url = "mongodb+srv://rvan3:vanraymond@nba-ml-xmtqa.mongodb.net/nba?retryWrites=true";
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  MongoClient.connect(url, function(err, client) {
+    if (err) throw err;
+    var db = client.db("nba");
+    db.collection("predictions").find({"_id": ObjectId("5c60f86abeb2772ce4c76a7f")}, {projection:{ _id: 0 }}).toArray(function(err, result) {
+      if (err) throw err;
+      // console.log(Object.keys(result[0]));
+      res.render('index', { data: result[0] });
+    });
+  });
+});
+  
+app.listen(3000, () => {
+console.log('App listening on port 3000!')
+});
