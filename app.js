ed52430abb8a599284509c981a628dd854b3daa5
raymond-van <vraymond028@gmail.com>
2019-02-12 00:42:36 -0500

    Added procfile and more changes for heroku deployment

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ app.get('/', (req, res) => {
     });
   });
 });
-  
-app.listen(3000, () => {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
 console.log('App listening on port 3000!')
 });
